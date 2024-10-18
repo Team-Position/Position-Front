119f44262d5ac5572d76e1a5b193cb42acc67d06
Seok-SangHun <amen101673@gmail.com>
2024-10-19 01:29:40 +0900

    resume-write updateing

--- static/js/my-page/my-employment-status.js
+++ static/js/my-page/my-employment-status.js
@@ -10,7 +10,7 @@ btn.addEventListener("click", () => {
 });
 
 document.addEventListener("DOMContentLoaded", () => {
-    const applicantHistoryBtns = document.querySelectorAll(".btn_history");
+    const applicantHistoryBtns = document.querySelectorAll(".btn-history");
 
     applicantHistoryBtns.forEach((btn) => {
         btn.addEventListener("click", () => {
@@ -21,7 +21,7 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 });
 
-const hideButton = document.getElementById("list_hide_btn");
+const hideButton = document.getElementById("list-hide-btn");
 const tipContent = document.querySelector(".TipCont.TopLeft");
 
 tipContent.style.display = "none";
@@ -35,58 +35,58 @@ hideButton.addEventListener("mouseout", () => {
 });
 
 const applyData = [
-    // {
-    //     id: 1,
-    //     date: "2024.10.17 16:42",
-    //     company: "마린웍스㈜",
-    //     position: "플랫폼 서비스 개발",
-    //     description: "IoT, 제어, 빅데이터 및 C# 기반 어플리케이션 개발자 채용",
-    //     status: "지원완료",
-    //     viewStatus: "미열람",
-    // },
-    // {
-    //     id: 2,
-    //     date: "2024.10.17 16:42",
-    //     company: "마린웍스㈜",
-    //     position: "플랫폼 서비스 개발",
-    //     description: "IoT, 제어, 빅데이터 및 C# 기반 어플리케이션 개발자 채용",
-    //     status: "지원취소",
-    //     viewStatus: "미열람",
-    // },
-    // {
-    //     id: 3,
-    //     date: "2024.10.17 16:42",
-    //     company: "마린웍스㈜",
-    //     position: "플랫폼 서비스 개발",
-    //     description: "IoT, 제어, 빅데이터 및 C# 기반 어플리케이션 개발자 채용",
-    //     status: "지원대기",
-    //     viewStatus: "열람",
-    // },
+    {
+        id: 1,
+        date: "2024.10.17 16:42",
+        company: "마린웍스㈜",
+        position: "플랫폼 서비스 개발",
+        description: "IoT, 제어, 빅데이터 및 C# 기반 어플리케이션 개발자 채용",
+        status: "지원완료",
+        viewStatus: "미열람",
+    },
+    {
+        id: 2,
+        date: "2024.10.17 16:42",
+        company: "마린웍스㈜",
+        position: "플랫폼 서비스 개발",
+        description: "IoT, 제어, 빅데이터 및 C# 기반 어플리케이션 개발자 채용",
+        status: "지원취소",
+        viewStatus: "미열람",
+    },
+    {
+        id: 3,
+        date: "2024.10.17 16:42",
+        company: "마린웍스㈜",
+        position: "플랫폼 서비스 개발",
+        description: "IoT, 제어, 빅데이터 및 C# 기반 어플리케이션 개발자 채용",
+        status: "지원대기",
+        viewStatus: "열람",
+    },
 ];
 
-const listContainer = document.querySelector(".list_status");
+const listContainer = document.querySelector(".list-status");
 const pageBox = document.querySelector(".PageBox");
 
 if (applyData.length > 0) {
     applyData.forEach((data) => {
         const row = document.createElement("div");
-        row.classList.add("row", "_apply_list", "open");
-        row.id = `apply_list_${data.id}`;
+        row.classList.add("row", "-apply-list", "open");
+        row.id = `apply-list-${data.id}`;
 
         row.innerHTML = `
             <div class="InpBox">
                 <span class="Chk Hide">
-                    <input type="checkbox" id="chk_${data.id}" value="${data.id}" class="_recruitapply_chk checkbox_idx" />
-                    <label class="Lbl" for="chk_${data.id}">선택</label>
+                    <input type="checkbox" id="chk-${data.id}" value="${data.id}" class="-recruitapply-chk checkbox-idx" />
+                    <label class="Lbl" for="chk-${data.id}">선택</label>
                 </span>
             </div>
-            <div class="col_date">${data.date}</div>
-            <div class="col_summary">
+            <div class="col-date">${data.date}</div>
+            <div class="col-summary">
                 <strong class="corp">
-                    <a href="#" target="_blank">${data.company}</a>
+                    <a href="#" target="-blank">${data.company}</a>
                 </strong>
                 <div class="recruit">
-                    <a href="#" target="_blank">
+                    <a href="#" target="-blank">
                         <span class="division">${data.position}</span>
                         <div class="TipBox">
                             <span>${data.description}</span>
@@ -96,41 +96,41 @@ if (applyData.length > 0) {
                  <div class="attached">
                                                 <button
                                                     type="button"
-                                                    class="data _file_down_resume"
+                                                    class="data -file-down-resume"
                                                    "
                                                 >
                                                     이수증 다운받기
                                                 </button>
                                             </div>
                                             <div class="status">
-                                                <em class="txt_status">
+                                                <em class="txt-status">
                                                     ${data.status}
                                                 </em>
-                                                <span class="txt_sub"
+                                                <span class="txt-sub"
                                                     >${data.viewStatus}</span
                                                 >
                                                 <button
                                                     type="button"
-                                                    class="btn_report _ai_report"
+                                                    class="btn-report -ai-report"
                                                   "
                                                 >
                                                     경쟁력분석
                                                 </button>
                                             </div>
             </div>
-            <div class="col_btns">
-                <div class="action"><span class="date_end"></span></div>
-                <button type="button" class="BtnType SizeM _apply_cancel">지원취소</button>
-                <button type="button" class="btn_history _applicant_history" data-id="${data.id}">지원내역</button>
+            <div class="col-btns">
+                <div class="action"><span class="date-end"></span></div>
+                <button type="button" class="BtnType SizeM -apply-cancel">지원취소</button>
+                <button type="button" class="btn-history -applicant-history" data-id="${data.id}">지원내역</button>
             </div>
-            <div class="col_history" id="history_${data.id}" style="display: none;">
+            <div class="col-history" id="history-${data.id}" style="display: none;">
                 <ol class="timeline">
                     <li class="now">
                         <span class="date">${data.date}</span>
                         <span class="desc">
                             <strong>지원 완료</strong>
                             <span>지원서류<button type="button" class="txt" onclick="viewApplyResume(${data.id}, 'print');">이력서</button></span>
-                            <span>지원 완료<button type="button" class="txt" onclick="cancelLayer('.cancel_layerWrap', 'on', '${data.company}', ${data.id});">지원 취소</button></span>
+                            <span>지원 완료<button type="button" class="txt" onclick="cancelLayer('.cancel-layerWrap', 'on', '${data.company}', ${data.id});">지원 취소</button></span>
                         </span>
                     </li>
                 </ol>
@@ -140,19 +140,19 @@ if (applyData.length > 0) {
         listContainer.appendChild(row);
     });
 
-    document.querySelectorAll(".btn_history").forEach((button) => {
+    document.querySelectorAll(".btn-history").forEach((button) => {
         button.addEventListener("click", (event) => {
             const id = event.target.getAttribute("data-id");
-            const history = document.getElementById(`history_${id}`);
+            const history = document.getElementById(`history-${id}`);
             history.style.display =
                 history.style.display === "block" ? "none" : "block";
         });
     });
 } else {
     listContainer.innerHTML = `
-         <form name="list_form_no_row" id="list_form_no_row">
-                                <div class="list_status">
-                                    <div class="no_row">
+         <form name="list-form-no-row" id="list-form-no-row">
+                                <div class="list-status">
+                                    <div class="no-row">
                                         <p class="txt">
                                             <b>입사지원 내역이 없어요</b>
                                             userName님에게 맞는 공고를
@@ -160,7 +160,7 @@ if (applyData.length > 0) {
                                         </p>
                                         <a
                                             href=""
-                                            class="link_go"
+                                            class="link-go"
                                             >나에게 맞는 공고 보러가기</a
                                         >
                                     </div>
@@ -172,7 +172,7 @@ if (applyData.length > 0) {
 
 const selectAll = document.getElementById("selectAll");
 const individualCheckboxes = document.querySelectorAll(
-    'input[type="checkbox"].checkbox_idx'
+    'input[type="checkbox"].checkbox-idx'
 );
 
 // 전체선택 체크박스 클릭 이벤트
